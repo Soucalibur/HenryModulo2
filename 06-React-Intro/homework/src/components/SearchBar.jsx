68d15bbc7a7a416cf28c37ecae54ccb5e3115104
Matthew Gallar <matthewgallar2112@outlook.com>
2022-09-05 21:58:10 -0300

    RI v0.1

--- 06-React-Intro/homework/src/components/SearchBar.jsx
+++ 06-React-Intro/homework/src/components/SearchBar.jsx
@@ -2,5 +2,17 @@ import React from 'react';
 
 export default function SearchBar(props) {
   // acá va tu código
-  return <div>Search Bar Component</div>
+  return (
+  
+  <div>
+    
+    <button onClick= {props.onSearch} > Agregar </button>
+    <input type="text" placeholder="Ciudad..."/>
+    
+
+  </div>
+  )
 };
+
+
+
